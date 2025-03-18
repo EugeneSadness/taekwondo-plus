ee2414cdfac85038cdcd2674c6292926d4868851
EugeneDamm <93585717+EugeneSadness@users.noreply.github.com>
2025-03-18 15:27:16 +0600

    Добавление GlobalStyle.ts

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #333;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul, ol {
+    list-style: none;
+  }
+`;
