59e3b78910f2bbc6defdd6936b356d1d283344e9
EugeneDamm <93585717+EugeneSadness@users.noreply.github.com>
2025-03-18 15:28:19 +0600

    Добавление компонента Hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,40 @@
+import { Container, SubTitle, Title, Description, ButtonsContainer, StatsContainer, StatsItem, StatsNumber, StatsText } from './HeroStyles';
+import { PrimaryButton, SecondaryButton } from '@/components/UI/Button/Button';
+
+const Hero = () => {
+  return (
+    <Container>
+      <SubTitle>ПУТЬ К СОВЕРШЕНСТВУ</SubTitle>
+      <Title>Школа тхэквондо для всех возрастов</Title>
+      <Description>
+        Профессиональные тренеры, современное оборудование и индивидуальный
+        подход к каждому ученику. Начните свой путь к мастерству сегодня!
+      </Description>
+      <ButtonsContainer>
+        <SecondaryButton to="/trainers">Наши тренеры</SecondaryButton>
+        <PrimaryButton to="/sign-up">Записаться на тренировку</PrimaryButton>
+      </ButtonsContainer>
+
+      <StatsContainer>
+        <StatsItem>
+          <StatsNumber>10+</StatsNumber>
+          <StatsText>Лет опыта</StatsText>
+        </StatsItem>
+        <StatsItem>
+          <StatsNumber>500+</StatsNumber>
+          <StatsText>Учеников</StatsText>
+        </StatsItem>
+        <StatsItem>
+          <StatsNumber>15+</StatsNumber>
+          <StatsText>Тренеров</StatsText>
+        </StatsItem>
+        <StatsItem>
+          <StatsNumber>50+</StatsNumber>
+          <StatsText>Наград</StatsText>
+        </StatsItem>
+      </StatsContainer>
+    </Container>
+  );
+};
+
+export default Hero;
